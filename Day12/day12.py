1c4ee246e74d81cb95690dfe9a445dce98425e04
David Xue <David.Xue37@myhunter.cuny.edu>
2023-01-14 16:57:58 -0500

    Day 12 part 1

---
+++ Day12/day12.py
@@ -0,0 +1,65 @@
+def shortest(file):
+    visited = {}
+    grid = [[]]
+    steps = 0
+    with open(file) as f:
+        content = f.readlines()
+        for line in content:
+            line = line
+            for l in line:
+                if(l=='\n'):
+                    grid.append([])
+                else:
+                    grid[-1].append(l)
+        startingrow = 0
+        startingcol = 0
+        endingrow = 0
+        endingcol = 0
+        grid.pop()
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if(grid[i][j]=='S'):
+                    startingrow=i
+                    startingcol=j
+                    grid[i][j]='a'
+                elif(grid[i][j]=='E'):
+                    endingrow=i
+                    endingcol = j
+                    grid[i][j] = 'z'
+        return path(grid,visited,startingrow,startingcol,endingrow,endingcol)
+
+def path(grid,visited,startrow,startcol,endrow,endcol):
+    queue = [(startrow,startcol)]
+    visited = {}
+    distance = 0
+    while(len(queue)!=0):
+        temp_len = len(queue)
+        #print(queue)
+        for i in range(temp_len):
+            row = queue[i][0]
+            col = queue[i][1]
+            if(row+1>=0 and row+1<len(grid) and (row+1,col) not in visited):
+                if(ord(grid[row+1][col])<=ord(grid[row][col])+1):
+                    if((row+1,col) not in queue):
+                        queue.append((row+1,col))
+            if(row-1>=0 and row-1<len(grid) and (row-1,col) not in visited):
+                if(ord(grid[row-1][col])<=ord(grid[row][col])+1):
+                    if((row-1,col) not in queue):
+                        queue.append((row-1,col))
+            if(col+1>=0 and col+1<len(grid[0]) and (row,col+1) not in visited):
+                if(ord(grid[row][col+1])<=ord(grid[row][col])+1):
+                    if((row,col+1) not in queue):
+                        queue.append((row,col+1))
+            if(col-1>=0 and col-1<len(grid[0]) and (row,col-1) not in visited):
+                if(ord(grid[row][col-1])<=ord(grid[row][col])+1):
+                    if((row,col-1) not in queue):
+                        queue.append((row,col-1))
+            visited[(row,col)] = 1
+        distance +=1
+        if((endrow,endcol) in queue):
+            return distance
+        for i in range(temp_len):
+            queue.pop(0)
+        
+
+print(shortest("shortest.txt"))
